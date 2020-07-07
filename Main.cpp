dd652cec5704b60cdde318bf76c0b29f9a071ec3
austin nelson <nelsonhaustin@gmail.com>
2020-07-07 01:00:19 -0700

    SFML Compiles!

---
+++ Main.cpp
@@ -0,0 +1,20 @@
+#include "SFML/Graphics.hpp"
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(512, 307), "Billiards");
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        window.clear(sf::Color::Magenta);
+        window.display();
+    }
+    return 0;
+}
+iefgjefjeofj
